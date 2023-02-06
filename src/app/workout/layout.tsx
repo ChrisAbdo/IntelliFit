bf6afb45ea583484ba538c80f111471586a8a403
chris abdo <chris.j.abdo@gmail.com>
2023-02-06 01:12:07 -0500

    ADDED: Should be fixed.

--- src/app/workout/layout.tsx
+++ src/app/workout/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
-import Navbar from '../components/Navbar';
 
 export default function RootLayout({
   children,
@@ -9,16 +8,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>
-        <Navbar />
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
+    <main>
+      <ThemeProvider>{children}</ThemeProvider>
+    </main>
   );
 }
